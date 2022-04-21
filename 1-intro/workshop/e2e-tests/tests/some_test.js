902d30896ddfb29aae2953a8762df56e524fb47f
Lightuky <47103897+Lightuky@users.noreply.github.com>
2022-04-21 17:05:49 +0200

    add end to end test

--- 1-intro/workshop/e2e-tests/tests/some_test.js
+++ 1-intro/workshop/e2e-tests/tests/some_test.js
@@ -1,5 +1,10 @@
 Feature('Ynov Nantes');
 
-Scenario('Test Ynov Nantes Land Page', ({ I }) => {
+Scenario('Test Ynov Nantes Recherche', async ({I}) => {
     I.amOnPage('https://www.ynov-nantes.com/');
+    I.click('.search-cta a');
+    I.fillField({ css: '.search-form-input .searchfield' }, 'Info');
+    I.waitForElement('.search-preview-cursus .preview-title .tile-title', 2);
+    let tile = locate('.search-preview-cursus .preview-title .tile-title').withText('Bachelor Informatique');
+    I.seeElement(tile);
 });
