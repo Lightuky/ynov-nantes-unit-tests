a03e061e99ad0d53122426a0053ca515659b1016
Lightuky <47103897+Lightuky@users.noreply.github.com>
2022-04-21 17:06:10 +0200

    add partial integration tests

--- 1-intro/workshop/integration-tests/Readme.md
+++ 1-intro/workshop/integration-tests/Readme.md
@@ -4,7 +4,7 @@ Dans votre terminal, dans ce répertoire lancer la commande :
 
 ``` docker-compose up --build  ```
 
-Un serveur **mongodb** est lancé pour le bon fonctionement du projet.
+Un serveur **mongodb** est lancé pour le bon fonctionnement du projet.
 
 Un serveur **node** sera accessible sur le port 3333 et vous pouvez consulter sur votre navigateur avec l'url ``` localhost:3333 ```
 
--- 1-intro/workshop/integration-tests/src/models/Item.js
+++ 1-intro/workshop/integration-tests/src/models/Item.js
@@ -2,14 +2,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ItemSchema = new Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  date: {
-    type: Date,
-    default: Date.now
-  }
+    name: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
 });
 
 module.exports = Item = mongoose.model('item', ItemSchema);
