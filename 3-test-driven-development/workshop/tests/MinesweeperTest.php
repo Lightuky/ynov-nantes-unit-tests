38e13482a535c895e2b05b8a42cf5c6cafb48139
Lightuky <47103897+Lightuky@users.noreply.github.com>
2022-04-28 16:04:39 +0200

    test: Changed tests with new propery / add normal behaviour test

--- 3-test-driven-development/workshop/tests/MinesweeperTest.php
+++ 3-test-driven-development/workshop/tests/MinesweeperTest.php
@@ -25,8 +25,8 @@ class MinesweeperTest extends TestCase
         $this->expectException(ArgumentCountError::class);
         $incomplete_fields = [
             new Field(linesCount: 4, columnsCount: 5),
-            new Field(columnsCount: 5, body: 14),
-            new Field(body: 8),
+            new Field(columnsCount: 5, bombsCount: 14),
+            new Field(bombsCount: 8),
             new Field(),
         ];
         $minesweeper = new Minesweeper($incomplete_fields);
@@ -37,12 +37,9 @@ class MinesweeperTest extends TestCase
     {
         $this->expectException(TypeError::class);
         $wrong_types_fields = [
-            new Field(4, 5, "string"),
-            new Field(4, 5, 15),
-            new Field(4, 5, 50.8),
-            new Field(null, null, []),
-            new Field("string", "string", []),
-            new Field(50.8, 50.8, []),
+            new Field(null, null, null),
+            new Field("string", "string", "string"),
+            new Field(50.8, 50.8, 50.8),
         ];
         $minesweeper = new Minesweeper($wrong_types_fields);
         $minesweeper->createMinesweeper();
@@ -52,12 +49,31 @@ class MinesweeperTest extends TestCase
     {
         $this->expectException(InvalidArgumentException::class);
         $extreme_values_fields = [
-            new Field(-3, 5, []),
-            new Field(105, 5, []),
-            new Field(4, -7, []),
-            new Field(4, 112, []),
+            new Field(-3, 5, 4),
+            new Field(105, 5, 4),
+            new Field(4, -7, 4),
+            new Field(4, 112, 4),
+            new Field(4, 5, -5),
+            new Field(4, 5, 25),
         ];
         $minesweeper = new Minesweeper($extreme_values_fields);
         $minesweeper->createMinesweeper();
+    }
+
+    public function testMinesweeperCreation(): void
+    {
+        $fieldsList = [];
+        $fields_data = [
+            ["lines_count" => 7, "columns_count" => 4, "bombs_count" => 5],
+            ["lines_count" => 6, "columns_count" => 3, "bombs_count" => 3],
+            ["lines_count" => 4, "columns_count" => 8, "bombs_count" => 6],
+            ["lines_count" => 7, "columns_count" => 7, "bombs_count" => 8],
+        ];
+        foreach ($fields_data as $field_data) {
+            $fieldsList[] = new Field($field_data['lines_count'], $field_data['columns_count'], $field_data['bombs_count']);
+        }
+        $minesweeper = new Minesweeper($fieldsList);
+        $minesweeper->createMinesweeper();
+
     }
 }
