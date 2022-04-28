88d8afabce519a674a4e4879ed3dd28586e63617
Lightuky <47103897+Lightuky@users.noreply.github.com>
2022-04-28 16:05:05 +0200

    feat: Create base logic for minesweeper board

--- 3-test-driven-development/workshop/src/Minesweeper.php
+++ 3-test-driven-development/workshop/src/Minesweeper.php
@@ -25,7 +25,20 @@ final class Minesweeper
                 break;
             } elseif ($field->linesCount < 0 || $field->columnsCount < 0 || $field->linesCount > 100 || $field->columnsCount > 100) {
                 throw new InvalidArgumentException("Lines/Columns count must be between 1 and 100");
+            } elseif ($field->bombsCount < 1 || $field->bombsCount > ($field->linesCount * $field->columnsCount) / 5) {
+                throw new InvalidArgumentException("Bombs count must be between 1 and 1/5 of the tiles");
             } else {
+                $board = [$field->linesCount . " " . $field->columnsCount];
+                $tileStr = str_repeat(".", $field->linesCount * $field->columnsCount);
+                $randBombIndexes = array_rand(range(0, ($field->linesCount * $field->columnsCount) - 1), $field->bombsCount);
+                $sliceLimit = 0;
+                foreach ($randBombIndexes as $randBombIndex) {
+                    $tileStr = substr_replace($tileStr, "*", $randBombIndex, 1);
+                }
+                for ($i = 0; $i < $field->linesCount; $i++) {
+                    $board[$i + 1] = substr($tileStr, $sliceLimit, $field->columnsCount);
+                    $sliceLimit = $sliceLimit + $field->columnsCount;
+                }
 
             }
         }
