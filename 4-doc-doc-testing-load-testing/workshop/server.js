66dd023ec7d4b774551beeb249ded4ee7d8e7921
Lightuky <47103897+Lightuky@users.noreply.github.com>
2022-04-29 14:34:47 +0200

    setup

--- 4-doc-doc-testing-load-testing/workshop/server.js
+++ 4-doc-doc-testing-load-testing/workshop/server.js
@@ -1,6 +1,6 @@
+const bodyParser = require("body-parser");
 const express = require('express');
 const path = require('path');
-const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 5000;
 const app = express();
